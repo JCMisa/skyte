44df9f3a74f11be20106ec4e43f5ebd83244f100
JCMisa <johncarlomisa399@gmail.com>
2026-02-09 17:07:39 +0800

    design the chat container to see sender and receiver chat bubbles

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import MessageInput from "./MessageInput";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
+  const messageEndRef = useRef(null);
+
   const messages = useChatStore((s) => s.messages);
   const getMessages = useChatStore((s) => s.getMessages);
   const isMessagesLoading = useChatStore((s) => s.isMessagesLoading);
@@ -24,7 +27,7 @@ const ChatContainer = () => {
       <div className="flex-1 flex flex-col overflow-auto">
         <ChatHeader />
         <MessageSkeleton />
-        {/* <MessageInput /> */}
+        <MessageInput />
       </div>
     );
   }
@@ -33,7 +36,43 @@ const ChatContainer = () => {
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
 
-      <p>messages...</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser?._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser?._id
+                      ? authUser?.profilePic || "/avatar.png"
+                      : selectedUser?.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="max-w-full rounded-md mb-2 bg-base-200/80"
+                />
+              )}
+              {message.text && <p className="whitespace-pre">{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
 
       <MessageInput />
     </div>
